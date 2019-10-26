4fcab16de6c95f2622b7c86958fad1a2c11d5be3
maylisw <maylisw31@gmail.com>
2019-10-26 14:40:46 -0400

    Set up recycler view for universrity list in college picker class

--- app/src/main/java/com/example/yhacks/CollegePicker.java
+++ app/src/main/java/com/example/yhacks/CollegePicker.java
@@ -4,10 +4,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class CollegePicker extends AppCompatActivity {
 
     private SharedPreferences sharedPref;
+    private RecyclerView collegeRecyclerView;
+    private GridLayoutManager layoutManager;
+    private UniversityAdapter universityDisplayAdapter;
+    private List<University> universityList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,13 @@ public class CollegePicker extends AppCompatActivity {
 
     private void wireWidgets() {
         //todo get university list + have a fill in your own
+        //collegeRecyclerView = findViewById(R.id.);
+        layoutManager = new GridLayoutManager(this, 1);
+        collegeRecyclerView.setLayoutManager(layoutManager);
+        collegeRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        universityDisplayAdapter = new UniversityAdapter(universityList, this);
+        collegeRecyclerView.setAdapter(universityDisplayAdapter);
+        registerForContextMenu(collegeRecyclerView);
 
     }
 }
